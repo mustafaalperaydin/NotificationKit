da047eb79ed51a38c8a818018300b5d6696f73b1
Alper <alperaydin@onuss.com>
2025-01-17 16:50:32 +0300

    Initial Commit

---
+++ NotificationKit/NotificationKit.h
@@ -0,0 +1,18 @@
+//
+//  NotificationKit.h
+//  NotificationKit
+//
+//  Created by Mustafa Alper Aydin on 17.01.2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NotificationKit.
+FOUNDATION_EXPORT double NotificationKitVersionNumber;
+
+//! Project version string for NotificationKit.
+FOUNDATION_EXPORT const unsigned char NotificationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NotificationKit/PublicHeader.h>
+
+
